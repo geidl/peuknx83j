15ae5be9e3bea3f033983fe1cac3690260d31506
geidl <138381981+geidl@users.noreply.github.com>
2023-07-03 10:42:07 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 ﻿#!/usr/bin/env bash
 
-UUID=${UUID:-'a3e79202-52bd-4e51-ab10-8f2eacb7756c'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-trojan'}
-SS_WSPATH=${SS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-shadowsocks'}
+UUID=${UUID:-'6779d7f0-db73-43f7-a246-4434ee092e59'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/6779d7f0-db73-43f7-a246-4434ee092e59-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/6779d7f0-db73-43f7-a246-4434ee092e59-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/6779d7f0-db73-43f7-a246-4434ee092e59-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/6779d7f0-db73-43f7-a246-4434ee092e59-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
